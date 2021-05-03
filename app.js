6bf3f40b3daed6024b333533b72399477cf97148
agilsurya <agilsurya5@gmail.com>
2021-05-03 09:07:52 +0700

    try connect to databases

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 const express = require('express');
-
+const mysql = require('mysql');
 const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 
 app.use(express.static('script'));
 
-app.get('/', (req, res) => {
-  res.render('hello.ejs');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'NewPassword',
+  database: 'lazizmu'
 });
 
-app.get('/index', (req, res) => {
-    res.render('index.ejs')
-})
+app.get('/', (req, res) => {
+  res.render('index.ejs', {
+    header_menus: [
+      {id: 1, name: 'Beranda', link: ''},
+      {id: 2, name: 'Tentang Kami', link: ''},
+      {id: 3, name: 'Program', link: ''},
+      {id: 4, name: 'Zakat Now', link: ''},
+      {id: 5, name: 'Masuk', link: ''}
+    ],
+    footer_menus: [
+      {id: 1, name: 'Program', link: ''},
+      {id: 2, name: 'Info', link: ''},
+      {id: 3, name: 'Kontak', link: ''}
+    ],
+    footer_1: [
+      {id: 1, name: 'Donasi', link: ''},
+      {id: 2, name: 'Zakat', link: ''},
+      {id: 3, name: 'Infak', link: ''},
+      {id: 3, name: 'Qurban', link: ''}
+    ],
+    footer_2: [
+      {id: 1, name: 'Tentang Kami', link: ''},
+      {id: 2, name: 'Tim Kami', link: ''},
+      {id: 3, name: 'Syarat dan Ketentuan', link: ''},
+      {id: 3, name: 'Dokumen', link: ''}
+    ]
+  });
+});
 
-app.listen(3000);
+
+
+app.listen(port);
---
+++ script/index_data.js
@@ -0,0 +1,6 @@
+const items = [
+    {id: 1, name: 'Donasi', link: ''},
+    {id: 2, name: 'Zakat', link: ''},
+    {id: 3, name: 'Infak', link: ''},
+    {id: 3, name: 'Qurban', link: ''}
+  ];
